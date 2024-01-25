1bac53c5990d71f27182df2557043fd962da69d8
Kitanist <eneskerem2516@gmail.com>
2024-01-25 21:30:53 +0300

    UI

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class House : MonoBehaviour
 {
-    
+    public string HouseName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(HouseName);
+        }
+       
+    }
+
+
+
+
 }

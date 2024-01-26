74f646a8d00f988e3db972a4512d516a513ede3c
Kitanist <eneskerem2516@gmail.com>
2024-01-26 07:47:28 +0300

    Final öncesi hali

--- Assets/Scripts/DialougeMaster.cs
+++ Assets/Scripts/DialougeMaster.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueSystemWithText;
 
+
 public class DialougeMaster : MonoSingeleton<DialougeMaster>
 {
     [SerializeField] DialogueUIController diyalogController;
-  
+    public CameraSmoother MainCamFollow;
+    public GameObject MainCam,pos1,pos2,pos3;
     [SerializeField]  private PlayerMovement Player;
     void Start()
     {
+        LeanTween.move(MainCam, pos1.transform, 20f);
+        Invoke(nameof(Loop),20f);
+    }
+    public void Loop()
+    {
+        LeanTween.move(MainCam, pos2.transform, 20f);
+        Invoke(nameof(Loop2), 20f);
+    }
+    public void Loop2()
+    {
+        LeanTween.move(MainCam, pos3.transform, 20f);
+
+        Invoke(nameof(StartDialouge), 20f);
+        Invoke(nameof(StartCamFollow), 20f);
+    }
+    
+    public void StartDialouge()
+    {
         diyalogController.ShowDialogueUI();
     }
-
    public void StopToDialogueEnd()
     {
         Player.CanMove = false;
@@ -20,6 +39,14 @@ public class DialougeMaster : MonoSingeleton<DialougeMaster>
     public void DialogueEnd()
     {
         Player.CanMove = true;
+    }
+    public void StopCamFollow()
+    {
+        MainCamFollow.enabled = false;
+    }
+    public void StartCamFollow()
+    {
+        MainCamFollow.enabled = true;
     }
    
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioClip StartMusic,NormalMusic;
+    public AudioSource SourceAudio;
+    public GameObject Envanter;
+    public GameObject ESC;
+    public GameObject SkillTree;
+    public GameObject QuestUI;
+    public Slider HealthBar;
+    public float Health;
+    private void Start()
+    {
+        Health = 1;
+        SourceAudio.PlayOneShot(StartMusic);
+    }
+    public void StopMusic()
+    {
+        SourceAudio.Stop();
+    }
+    public void StartNormalMusic()
+    {
+        SourceAudio.PlayOneShot(NormalMusic);
+    }
+    void Update()
+    {
+        HealthBar.value = Health;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Envanter.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ESC.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            SkillTree.SetActive(true);
+        }
+    }
+    public void CloseUI()
+    {
+        SkillTree.SetActive(false);
+        ESC.SetActive(false);
+        Envanter.SetActive(false);
+    }
+    public void OpenQuestUI()
+    {
+        QuestUI.SetActive(true);
+    }
+    public void CloseQuestUI()
+    {
+        QuestUI.SetActive(false);
+    }
+}

564d4ecb60e5de2a1e9683e51350aa7319e86b67
Kitanist <eneskerem2516@gmail.com>
2024-04-11 20:21:53 +0300

    Vize öncesi zarttiri zort

--- Assets/Scripts/DialougeMaster.cs
+++ Assets/Scripts/DialougeMaster.cs
@@ -6,26 +6,28 @@ using DialogueSystemWithText;
 
 public class DialougeMaster : MonoSingeleton<DialougeMaster>
 {
-    [SerializeField] DialogueUIController diyalogController;
+    [SerializeField] DialogueUIController diyalogController,diyalogStarter;
     public CameraSmoother MainCamFollow;
     public GameObject MainCam,pos1,pos2,pos3;
     [SerializeField]  private PlayerMovement Player;
     void Start()
     {
-        LeanTween.move(MainCam, pos1.transform, 20f);
-        Invoke(nameof(Loop),20f);
+        
+        LeanTween.move(MainCam, pos1.transform, 10f);
+        Invoke(nameof(Loop),10f);
     }
     public void Loop()
     {
-        LeanTween.move(MainCam, pos2.transform, 20f);
-        Invoke(nameof(Loop2), 20f);
+        diyalogStarter.ShowDialogueUI();
+        LeanTween.move(MainCam, pos2.transform, 10f);
+        Invoke(nameof(Loop2), 10f);
     }
     public void Loop2()
     {
-        LeanTween.move(MainCam, pos3.transform, 20f);
+        LeanTween.move(MainCam, pos3.transform, 10f);
 
-        Invoke(nameof(StartDialouge), 20f);
-        Invoke(nameof(StartCamFollow), 20f);
+        Invoke(nameof(StartDialouge), 10f);
+        Invoke(nameof(StartCamFollow), 10f);
     }
     
     public void StartDialouge()
